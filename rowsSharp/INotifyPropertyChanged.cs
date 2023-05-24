dc2873a5f97cdb929881effae3d9455a93a04c86
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-05-24 10:42:53 +0800

    Relocate INotifyPropertyChanged implementation

--- rowsSharp/INotifyPropertyChanged.cs
+++ rowsSharp/INotifyPropertyChanged.cs
@@ -3,12 +3,12 @@ using System.Runtime.CompilerServices;
 
 namespace rowsSharp;
 
-public abstract class INPC : INotifyPropertyChanged
+public abstract class NotifyPropertyChanged : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
     public void OnPropertyChanged(string propertyName)
     {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        PropertyChanged?.Invoke(this, new(propertyName));
     }
 
     private protected void SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
