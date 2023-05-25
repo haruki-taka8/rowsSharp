2b624741b8f8e2b697dbd05dce25933eb2e6405c
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-05-25 16:58:08 +0800

    Simplify exception event handler

--- rowsSharp/App.xaml.cs
+++ rowsSharp/App.xaml.cs
@@ -5,19 +5,17 @@ namespace rowsSharp;
 
 public partial class App : Application
 {
-    internal static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+    internal static NLog.Logger Logger => NLog.LogManager.GetCurrentClassLogger();
 
     private App()
     {
-        AppDomain.CurrentDomain.UnhandledException += new(Handler);
+        AppDomain.CurrentDomain.UnhandledException += Handler;
     }
 
     private void Handler(object sender, UnhandledExceptionEventArgs args)
     {
-        var exception = (Exception)args.ExceptionObject;
-
         MessageBox.Show(
-           "RowsSharp will close due to the following exception:\n\n" + exception.Message + "\n\n" + exception.StackTrace,
+           "RowsSharp will close due to the following error:\n\n" + args.ExceptionObject,
            "RowsSharp",
            MessageBoxButton.OK,
            MessageBoxImage.Error
