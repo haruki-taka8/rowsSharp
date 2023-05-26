f58a047be4daa792811e9098db5b0cd39673655a
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-05-26 17:33:38 +0800

    Introduce Welcome view

---
+++ rowsSharp/View/Welcome.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace rowsSharp.View;
+/// <summary>
+/// Interaction logic for Welcome.xaml
+/// </summary>
+public partial class Welcome : UserControl
+{
+    public Welcome()
+    {
+        InitializeComponent();
+    }
+}
