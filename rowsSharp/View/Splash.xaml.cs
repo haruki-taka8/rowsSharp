7121719fb1d70d291543b69c8a4a1952f555d74a
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-05-26 18:20:39 +0800

    Introduce Splash view

---
+++ rowsSharp/View/Splash.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace rowsSharp.View;
+
+/// <summary>
+/// Interaction logic for Splash.xaml
+/// </summary>
+public partial class Splash : UserControl
+{
+    public Splash()
+    {
+        InitializeComponent();
+    }
+}
