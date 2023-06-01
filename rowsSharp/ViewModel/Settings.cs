da490100e7fda502bfbb1dc06b17bb5a9390385d
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-06-01 10:43:36 +0800

    ViewModels for the new UserControls

---
+++ rowsSharp/ViewModel/Settings.cs
@@ -0,0 +1,17 @@
+﻿ using System.Windows.Controls;
+
+namespace rowsSharp.ViewModel;
+
+public class SettingsVM : NotifyPropertyChanged
+{
+    private readonly RootVM rootVM;
+    private readonly UserControl previous;
+
+    public SettingsVM(RootVM rootViewModel, UserControl previousUserControl)
+    {
+        rootVM = rootViewModel;
+        previous = previousUserControl;
+    }
+
+    public DelegateCommand Return => new(() => rootVM.CurrentUserControl = previous);
+}
