a92b072ac060b744d62a46f56e0e4c926dc3fb46
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-06-01 10:51:12 +0800

    Editor ViewModel

---
+++ rowsSharp/ViewModel/Editor/Filter.cs
@@ -0,0 +1,75 @@
+﻿using ObservableTable.Core;
+using rowsSharp.Model;
+using rowsSharp.View;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace rowsSharp.ViewModel.Editor;
+
+public class Filter : NotifyPropertyChanged
+{
+    private readonly RootVM rootVM;
+    private Preferences Preferences => rootVM.Preferences;
+    private ObservableTable<string> Table => rootVM.Table;
+    private ICollectionView collectionView { get; set; }
+
+    private readonly Domain.Filter filter;
+
+    public Filter(RootVM rootViewModel, ICollectionView collectionView)
+    {
+        rootVM = rootViewModel;
+
+        this.collectionView = collectionView;
+        filter = new(collectionView);
+        filter.Alias = Preferences.ColumnStyle?.Alias ?? new();
+    }
+
+    private string filterText = "";
+    public string FilterText
+    {
+        get => filterText;
+        set => SetField(ref filterText, value);
+    }
+
+    public DelegateCommand InvokeFilter => new(() =>
+    {
+        filter.UseRegex = Preferences.UseRegexFilter;
+        filter.UseInputAlias = Preferences.UseInputAlias;
+        filter.UseOutputAlias = Preferences.IsReadOnly && Preferences.UseOutputAlias;
+        filter.Headers = Table.Headers;
+        filter.FilterText = FilterText;
+        collectionView = filter.Invoke();
+    });
+
+    private bool? isFilterFocused;
+    public bool? IsFilterFocused
+    {
+        get => isFilterFocused;
+        set => SetField(ref isFilterFocused, value);
+    }
+
+    public DelegateCommand FocusFilter => new(
+        () =>
+        {
+            IsFilterFocused = false;
+            IsFilterFocused = true;
+        }
+    );
+
+    public DelegateCommand OpenPreferences => new(() =>
+    {
+        rootVM.SettingsVM = new(rootVM, rootVM.CurrentUserControl);
+        rootVM.CurrentUserControl = new Settings();
+    });
+
+    public DelegateCommand<DataGrid> ResetSorting => new(
+        (datagrid) =>
+        {
+            collectionView.SortDescriptions.Clear();
+            foreach (var column in datagrid.Columns)
+            {
+                column.SortDirection = null;
+            }
+        }
+    );
+}
