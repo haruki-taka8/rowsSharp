4c4e517a287a14d6dee1468af4f565fa6482f8e9
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-06-02 10:35:39 +0800

    Relocate CSV Im/Export business logic

---
+++ rowsSharp/Domain/CsvFile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using ObservableTable.Core;
+using ObservableTable.IO;
+using System.IO;
+
+namespace rowsSharp.Domain;
+
+internal class CsvFile
+{
+    internal static ObservableTable<string> Import(string path, bool hasHeader)
+    {
+        if (!File.Exists(path)) { return new(); }
+
+        return Importer.FromFilePath(path, hasHeader);
+    }
+
+    private static string RequestFilePath()
+    {
+        SaveFileDialog dialog = new()
+        {
+            Filter = "Comma-seperated values (*.csv)|*.csv|All files (*.*)|*.*",
+            DefaultExt = "csv"
+        };
+
+        dialog.ShowDialog();
+
+        return dialog.FileName;
+    }
+
+    internal static void Export(string path, ObservableTable<string> table, bool hasHeader)
+    {
+        if (!File.Exists(path))
+        {
+            path = RequestFilePath();
+        }
+
+        Exporter.ToFile(path, table, hasHeader);
+    }
+}
