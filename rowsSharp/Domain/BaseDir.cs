59aa96de43d3007c5e71516396893b4b44744c01
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-06-03 18:51:38 +0800

    Introduce static BaseDir class to abstract logic

---
+++ rowsSharp/Domain/BaseDir.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace rowsSharp.Domain;
+
+internal static class BaseDir
+{
+    private const string Metavariable = "$baseDir";
+    private static readonly string BasePath = Environment.CurrentDirectory + "/Userdata/";
+
+    internal static string Expand(string path)
+    {
+        return path.Replace(Metavariable, BasePath);
+    }
+}
