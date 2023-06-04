5801ba5af5961cdd6d8d4defa88dea65e3502c4a
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-06-04 09:35:21 +0800

    Introduce UniqueColumn business logic class

---
+++ rowsSharp/Domain/UniqueColumn.cs
@@ -0,0 +1,22 @@
+﻿using ObservableTable.Core;
+using System.Collections.Generic;
+
+namespace rowsSharp.Domain;
+
+internal class UniqueColumn
+{
+    private int index;
+
+	private static Column<string> GetNumberedColumn(int index)
+	{
+		return new(index.ToString());
+	}
+
+	internal IEnumerable<Column<string>> Next(int count = 1)
+	{
+		for (int i = 0; i < count; i++)
+		{
+			yield return GetNumberedColumn(index++);
+		}
+	}
+}
