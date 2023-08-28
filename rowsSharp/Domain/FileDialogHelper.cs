f53e19938f6e8aa9fc2b08831460a7941ddfc151
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-08-28 21:07:18 +0800

    Centralize IO dialog requests

---
+++ rowsSharp/Domain/FileDialogHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+
+namespace RowsSharp.Domain;
+
+public static class FileDialogHelper
+{
+    private const string Filter = "Comma-separated values (*.csv)|*.csv|All files (*.*)|*.*";
+    private const string CsvExtension = "csv";
+
+    private static string RequestPath(FileDialog dialog)
+    {
+        dialog.Filter = Filter;
+        dialog.DefaultExt = CsvExtension;
+
+        dialog.ShowDialog();
+
+        return dialog.FileName;
+    }
+
+    public static string RequestReadPath()
+    {
+        return RequestPath(new OpenFileDialog());
+    }
+
+    public static string RequestWritePath()
+    {
+        return RequestPath(new SaveFileDialog());
+    }
+}
