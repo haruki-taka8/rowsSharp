bc0d5ce23469aa75b0d5b1a9cc68af31fd855088
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-08-28 21:05:41 +0800

    New ClipboardHelper class to ease ViewModel workload

---
+++ rowsSharp/Domain/Clipboard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Windows;
+
+namespace RowsSharp.Domain;
+
+public static class ClipboardHelper
+{
+    public static void SetFileFromPath(string path)
+    {
+        StringCollection list = new() { path };
+
+        Clipboard.SetFileDropList(list);
+    }
+
+    public static string[,] SplitTable()
+    {
+        string clipboard = Clipboard.GetText()
+                                    .ReplaceLineEndings();
+
+        string[] fields = clipboard.Split(new string[] { Environment.NewLine, "\t" }, StringSplitOptions.None);
+
+        int rowCount = clipboard.Split(Environment.NewLine).Length;
+        int columnCount = fields.Length / rowCount;
+
+        string[,] result = new string[rowCount, columnCount];
+
+        for (int y = 0; y < rowCount; y++)
+        {
+            for (int x = 0; x < columnCount; x++)
+            {
+                result[y, x] = fields[y * columnCount + x];
+            }
+        }
+
+        return result;            
+    }
+}
