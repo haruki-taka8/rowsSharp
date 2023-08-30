abedb81a1063709aeb0a489d7793db7d1ba372c9
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-08-30 16:56:13 +0800

    Auxiliary ValueConverters

---
+++ rowsSharp/View/MsecToSecConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RowsSharp.View;
+
+/// <summary>
+/// Convert a value from milliseconds to seconds, and round the result.
+/// </summary>
+
+public class MsecToSecConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return System.Convert.ToInt32(value) / 1000;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ rowsSharp/View/UserControlMultiConverter.cs
@@ -0,0 +1,36 @@
+﻿using RowsSharp.ViewModel;
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace RowsSharp.View;
+
+public class UserControlMultiConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length != 2
+            || values[0] is not Section section
+            || values[1] is not CommonViewModel commonViewModel
+        )
+        {
+            throw new ArgumentException(null, nameof(values));
+        }
+
+        return section switch
+        {
+            Section.Splash => new Splash(commonViewModel),
+            Section.Editor => new Editor(commonViewModel),
+            Section.Welcome => new Welcome(commonViewModel),
+            Section.Settings => new Settings(commonViewModel),
+
+            _ => throw new InvalidEnumArgumentException(nameof(section))
+        };
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
