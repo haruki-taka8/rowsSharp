89e2587f5066d9beed4b001d6fee9d45f4cf1e2c
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-09-02 11:02:41 +0800

    Use depedency injection

---
+++ rowsSharp/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RowsSharp.Model;
+
+namespace RowsSharp.ViewModel;
+
+public class SettingsViewModel
+{
+    public SettingsViewModel(CommonViewModel commonViewModel)
+    {
+        this.commonViewModel = commonViewModel;
+    }
+
+    private readonly CommonViewModel commonViewModel;
+
+    public Preferences Preferences => commonViewModel.Preferences;
+
+    public DelegateCommand Return => new(() =>
+        commonViewModel.CurrentSection = commonViewModel.PreviousSection
+    );
+}
