c4d4b0cb4b2ec0effb9bff1dc3af6f3d7ffeb707
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-09-27 18:16:28 +0800

    Converters because the new columns do not take strings

---
+++ rowsSharp/View/Converter/StringToUriConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RowsSharp.View;
+
+internal class StringToUriConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+        
+        if (value is not string text)
+        {
+            throw new ArgumentException(null, nameof(value));
+        }
+        
+        bool isValidUri = Uri.TryCreate(text, UriKind.RelativeOrAbsolute, out Uri? uri);
+        
+        return isValidUri ? uri! : text;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string or Uri)
+        {
+            throw new ArgumentException(null, nameof(value));
+        }
+
+        return value;
+    }
+}
