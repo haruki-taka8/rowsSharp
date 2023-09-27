163a0166ee156c50f60622bc609585b40c0ca3c4
Haruki Takahashi <77907336+haruki-taka8@users.noreply.github.com>
2023-09-27 18:14:39 +0800

    Base styling for the Hyperlink and CheckBox columns

---
+++ rowsSharp/View/ResourceDictionary/Hyperlink.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace RowsSharp.View;
+
+public partial class Hyperlink
+{        
+    public void Hyperlink_Clicked(object sender, RoutedEventArgs e)
+    {
+        var file = ((System.Windows.Documents.Hyperlink)e.OriginalSource)
+            .NavigateUri
+            .ToString();
+
+        StartShell(file);
+    }
+
+    private static void StartShell(string file)
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = file,
+            UseShellExecute = true,
+        };
+
+        try
+        {
+            Process.Start(info);
+        }
+        catch (Win32Exception)
+        {
+            // Invalid ProcessStartInfo.FileName
+        }
+    }
+}
